5620b11df74c757bb63396ff11bba80ae6272c18
dandyandy22 <andrasm96@gmail.com>
2021-12-23 10:28:13 -0800

    Create new Flutter app, seperate app, main and other files

---
+++ android/app/src/main/kotlin/com/example/redifit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.redifit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../screens/home.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RediFit',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(title: 'Demo Home Page'),
+    );
+  }
+}
