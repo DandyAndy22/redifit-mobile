bc47ceeccbc2f73ae7695ecefe17ed41dfc3f33a
dandyandy22 <andrasm96@gmail.com>
2022-01-11 20:40:56 -0800

    Add a table and switchtile to home screen #4

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,10 +8,59 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _state = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(),
+      body: Column(children: [
+        SwitchListTile(
+            title: const Text("Daily Target"),
+            value: _state,
+            onChanged: (bool value) {
+              setState(() {
+                _state = value;
+              });
+            }),
+        DataTable(
+          columns: const [
+            DataColumn(
+                label: Text('ID',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+            DataColumn(
+                label: Text('Name',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+            DataColumn(
+                label: Text('Profession',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+          ],
+          rows: const [
+            DataRow(cells: [
+              DataCell(Text('1')),
+              DataCell(Text('Stephen')),
+              DataCell(Text('Actor')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('5')),
+              DataCell(Text('John')),
+              DataCell(Text('Student')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('10')),
+              DataCell(Text('Harry')),
+              DataCell(Text('Leader')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('15')),
+              DataCell(Text('Peter')),
+              DataCell(Text('Scientist')),
+            ]),
+          ],
+        ),
+      ]),
     );
   }
 }
