175d5297713c0b8e8c4e3ba3da0c802b621fd0b6
dandyandy22 <andrasm96@gmail.com>
2022-02-02 14:41:19 -0800

    Add Content to Settings Drawer, format and style as well

--- lib/widgets/settings.dart
+++ lib/widgets/settings.dart
@@ -9,11 +9,52 @@ class Settings extends StatelessWidget {
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.orange.shade400,
-            ),
-            child: const Text('Settings'),
+          SizedBox(
+              height: 125,
+              child: DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.orange.shade400,
+                ),
+                child: const Center(
+                    child: Text('Settings',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 28))),
+              )),
+          ListTile(
+            title: const Text('Preferences',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('Account',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('What/why?',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: const Text('Help',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+              Navigator.pop(context);
+            },
           ),
         ],
       ),
