551ca4c9663c950c69a420048dfb19d9542f7b68
dandyandy22 <andrasm96@gmail.com>
2022-03-07 09:41:02 -0800

    Remove Firebase autho implementation related material

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,7 @@
 import 'package:flutter/material.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // FirebaseAuth auth = FirebaseAuth.instance;
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(const App());
-  // auth.authStateChanges().listen((User? user) {
-  //   if (user == null) {
-  //     print('User is currently signed out!');
-  //   } else {
-  //     print('User is signed in!');
-  //   }
-  // });
 }
