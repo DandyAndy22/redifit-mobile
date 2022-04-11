866605acc22c266c9ed3fe6ff611f1667593c572
Andras Mihaly <andrasm96@gmail.com>
2022-04-11 11:58:39 -0700

    Add Style file References #18

---
+++ lib/style.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Insets {
+  static const double xsmall = 3;
+  static const double small = 4;
+  static const double medium = 5;
+  static const double large = 10;
+  static const double extraLarge = 20;
+}
+
+class Fonts {
+  static const String raleway = 'Raleway';
+}
+
+class TextStyles {
+  static const TextStyle raleway = TextStyle(
+    fontFamily: Fonts.raleway,
+  );
+  static TextStyle buttonText1 =
+      const TextStyle(fontWeight: FontWeight.bold, fontSize: 14);
+  static TextStyle buttonText2 =
+      const TextStyle(fontWeight: FontWeight.normal, fontSize: 11);
+  static TextStyle h1 =
+      const TextStyle(fontWeight: FontWeight.bold, fontSize: 22);
+  static TextStyle h2 =
+      const TextStyle(fontWeight: FontWeight.bold, fontSize: 16);
+  static late TextStyle body1 =
+      raleway.copyWith(color: const Color(0xFF42A5F5));
+}
